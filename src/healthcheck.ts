bcf3852b3de4bf4734df6b38cdbfe55cf933a67f
Kiril Yakymchuk <stbestich@gmail.com>
2024-09-17 18:23:08 +0300

    feat: health check function

---
+++ src/healthcheck.ts
@@ -0,0 +1,32 @@
+import { Context } from "hono";
+import { pingCheck } from "./ping";
+
+type HealthCheck = {
+  uptime: number,
+  message: string,
+  status: number,
+  ping: { alive: boolean, time: number | "unknown" },
+  timestamp: Date;
+}
+
+export const healthcheck = async (c: Context) => {
+  const healthcheckData: HealthCheck = {
+    uptime: process.uptime(),
+    message: 'OK',
+    status: 200,
+    ping: { alive: false, time: "unknown" },
+    timestamp: new Date(),
+  };
+
+  try {
+    healthcheckData.ping = await pingCheck('localhost');
+    throw new Error();
+    return c.json(healthcheckData);
+  } catch (e) {
+    const error = e as Error;
+
+    healthcheckData.message = error?.message || 'Internal server error occured';
+    healthcheckData.status = 503;
+    return c.json(healthcheckData);
+  }
+}
